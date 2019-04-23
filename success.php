402e3ba9815a03e5fa22fc731b475c0b9ffc1007
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2019-04-23 08:51:27 +0100

    Update success.php

--- success.php
+++ success.php
@@ -3,8 +3,6 @@ include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../header.php');
 include(dirname(__FILE__).'/nochex.php');
 $id_cart = $_GET["id_cart"];
-// $id_cart = intval(Tools::getValue('id_cart', 0));
-// $id_module = intval(Tools::getValue('id_module', 0));
 
 $myid_order = Order::getOrderByCartId(intval($id_cart));
 $smarty->assign('nochexorder', $myid_order);
